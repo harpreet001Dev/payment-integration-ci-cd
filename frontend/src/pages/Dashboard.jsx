08a96cc964c2a067fa15feeaa07bb31fcbda6392
Harpreet <harpreet13108159@gmail.com>
2026-09-08 20:37:04 +0530

    aDS

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
     const [amount, setAmount] = useState(10);
     const [idempotencyKey, setIdempotencyKey] = useState(null);
 
-    //payment point asf asf
+    //payment point asf 
     const payAmount = async () => {
         try {
             let key = idempotencyKey;
