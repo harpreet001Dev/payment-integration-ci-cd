a237d48110b961a68087588f43399c0a6fe8d6d7
Harpreet <harpreet13108159@gmail.com>
2026-09-08 18:28:14 +0530

    added comment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        //connecting to db
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
