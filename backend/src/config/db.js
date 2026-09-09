7a6bf21e237d7cef1480f02f981763c2bf841cc1
Harpreet <harpreet13108159@gmail.com>
2026-09-09 10:57:19 +0530

    added cache for workflows

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    //checking db connections  
+    //checking db connections   asgv
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
